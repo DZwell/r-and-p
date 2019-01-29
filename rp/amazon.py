f5498d8a1ccf3b77ea2398292995ca717707cd9c
Daniel Zwelling <dzwellingmusic@gmail.com>
2019-01-28 22:50:59 -0800

    Find bsr in two html structures

--- rp/amazon.py
+++ rp/amazon.py
@@ -1,6 +1,6 @@
 
 def search_asin(browser, item_code):
-    browser.get('https://www.amazon.com/ref=nav_logo')
+    '''Search for asin'''
 
     search_field = browser.find_element_by_id('twotabsearchtextbox')
     click_search_button = str("javascript:document.getElementById('nav-search-submit-text').nextElementSibling.click()")
@@ -9,6 +9,33 @@ def search_asin(browser, item_code):
     search_field.send_keys(item_code)
     browser.execute_script(click_search_button)
     browser.execute_script(click_found_item_link)
-    x = browser.find_elements_by_xpath("//td/span/span/a")
-    import pdb; pdb.set_trace()
+
+    bsr = get_bsr(browser)
+    print('Item code: {}\nBest seller rank: {}'.format(item_code, bsr))
+
+
+def get_bsr(browser):
+    '''Return best seller rank'''
+
+    try:
+        li = browser.find_element_by_id('SalesRank')
+    except Exception:
+        li = None
+
+    try:
+        span_list = browser.find_elements_by_xpath("//span[contains(text(),'#')]")
+    except Exception:
+        span_list = None
+
+    if li:
+        li_text_list = li.text.split(' ')
+        return [string for string in li_text_list if '#' in string][0]
+    elif span_list:
+        return span_list[0].text.split(' ')[0]
+
+
+def clear_search_field(browser):
+    '''Clear search field'''
+    search_field = browser.find_element_by_id('twotabsearchtextbox')
+    search_field.clear()    
 
